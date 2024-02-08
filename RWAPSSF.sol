f32edbe5242ac60497cc4f1e8ad98e6d614ed6dc
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2024-02-08 16:55:42 +0700

    idx = 0 is  a default value now (no player id 0)

--- RWAPSSF.sol
+++ RWAPSSF.sol
@@ -24,15 +24,18 @@ contract RWAPSSF {
         numInput = 0;
 
         for(uint i = 0; i < 2; i++) {
-            player[i].choice = 0;
+            playersNumber[player[i].addr] = 0;
+            player[i].choice = 7;
             player[i].timestamp = 0;
-            player[i].playerNumber = 3;
+            player[i].playerNumber = 0;
             player[i].addr = address(0);
         }
     }
 
     function viewPlayer() public view returns(uint choice, uint timestamp, uint playerNumber, address addr){
-        Player memory temp = player[playersNumber[msg.sender]];
+        uint playerId = playersNumber[msg.sender];
+        require(playerId != 0);
+        Player memory temp = player[playerId];
         return (temp.choice, temp.timestamp, temp.playerNumber, temp.addr);
     }
 
@@ -43,19 +46,26 @@ contract RWAPSSF {
     function addPlayer() public payable {
         require(numPlayer < 2);
         require(msg.value == 1 ether);
+        require(playersNumber[msg.sender] == 0);
+
         reward += msg.value;
+        numPlayer++;
+        player[numPlayer].choice = 7;
         player[numPlayer].timestamp = block.timestamp;
         player[numPlayer].playerNumber = numPlayer;
         player[numPlayer].addr = msg.sender;
-
-        numPlayer++;
+        playersNumber[msg.sender] = numPlayer;
     }
 
     function input(uint choice) public  {
+        uint idx = playersNumber[msg.sender];
         require(numPlayer == 2);
         require(choice < 7);
-        player[playersNumber[msg.sender]].choice = choice;
+        require(idx != 0);
+
+        player[idx].choice = choice;
         numInput++;
+
         if (numInput == 2) {
             _checkWinnerAndPay();
         }
@@ -63,7 +73,6 @@ contract RWAPSSF {
 
     function withdraw() public payable {
         uint idx = playersNumber[msg.sender];
-        require(player[idx].addr == msg.sender);
         require(player[idx].timestamp + timeLimit < block.timestamp);
         require(numPlayer == 1 || numInput == 1);
         
@@ -72,8 +81,8 @@ contract RWAPSSF {
     }
 
     function _checkWinnerAndPay() private {
-        uint p0Choice = player[0].choice;
-        uint p1Choice = player[1].choice;
+        uint p0Choice = player[1].choice;
+        uint p1Choice = player[2].choice;
         address payable account0 = payable(player[0].addr);
         address payable account1 = payable(player[1].addr);
         if ((p0Choice + 1) % 7 == p1Choice || (p0Choice + 2) % 7 == p1Choice || (p0Choice + 3) % 7 == p1Choice) {
