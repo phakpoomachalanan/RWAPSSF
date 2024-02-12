113776ab8541e74f1825c661172eee2e9d163922
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2024-02-12 17:21:35 +0700

    without reveal from import

--- RWAPSSF.sol
+++ RWAPSSF.sol
@@ -5,8 +5,12 @@ pragma solidity >=0.7.0 <0.9.0;
 import "./CommitReveal.sol";
 
 contract RWAPSSF {
+    CommitReveal commitReveal = new CommitReveal();
+
     struct Player {
         uint choice; // 0 - Rock, 1 - Fire , 2 - Scissors, 3 - Sponge, 4 - Paper, 5 - Air, 6 - Water, 7 - Undefined
+        bytes32 commit;
+        bool isRevealed;
         uint timestamp;
         address addr;
     }
@@ -14,7 +18,8 @@ contract RWAPSSF {
     uint internal numPlayer = 0;
     uint internal reward = 0;
     uint internal numInput = 0;
-    uint internal timeLimit = 1 minutes;
+    uint internal numReveal = 0;
+    uint internal timeLimit = 10 minutes;
     mapping (uint => Player) private player;
     mapping (address => uint) private playersNumber;
 
@@ -22,24 +27,27 @@ contract RWAPSSF {
         numPlayer = 0;
         reward = 0;
         numInput = 0;
+        numReveal = 0;
         for(uint i = 0; i < 2; i++) {
             playersNumber[player[i].addr] = 0;
             player[i].choice = 7;
+            player[i].commit = bytes32(0);
+            player[i].isRevealed = false;
             player[i].timestamp = 0;
             player[i].addr = address(0);
         }
     }
 
-    function viewPlayer() public view returns(uint choice, uint timestamp, uint playerNumber, address addr){
+    function viewPlayer() public view returns(uint choice, bytes32 commit, bool isRevealed, uint timestamp, uint playerNumber, address addr){
         uint playerId = playersNumber[msg.sender];
         require(playerId != 0, "Registered player only");
 
         Player memory temp = player[playerId];
-        return (temp.choice, temp.timestamp, playerId, temp.addr);
+        return (temp.choice, temp.commit, temp.isRevealed, temp.timestamp, playerId, temp.addr);
     }
 
-    function viewGameStatus() public view returns(uint numberOfPlayer, uint gameReward, uint numberOfInput, uint canWithdrawAfter) {
-        return (numPlayer, reward, numInput, timeLimit);
+    function viewGameStatus() public view returns(uint numberOfPlayer, uint gameReward, uint numberOfInput, uint numberOfReveal, uint canWithdrawAfter) {
+        return (numPlayer, reward, numInput, numReveal, timeLimit);
     }
 
     function addPlayer() public payable {
@@ -55,16 +63,30 @@ contract RWAPSSF {
         playersNumber[msg.sender] = numPlayer;
     }
 
-    function input(uint choice) public  {
+    function input(uint choice, uint salt) public  {
         uint idx = playersNumber[msg.sender];
         require(idx != 0, "Registered player only");
         require(numPlayer == 2, "Player not enough");
         require(choice < 7, "Please select 0 - 6");
+        require(numInput < 2, "Wait for reveal");
+
+        player[idx].commit = commitReveal.getSaltedHash(bytes32(choice), bytes32(salt));
+        numInput++;
+        // commitReveal.commit(commitReveal.getSaltedHash(bytes32(choice), bytes32(salt)));
+    }
+
+    function revealChoice(uint choice, uint salt) public {
+        uint idx = playersNumber[msg.sender];
+        require(idx != 0, "Registered player only");
+        require(numInput == 2, "Wait for reveal");
+        require(player[idx].isRevealed == false, "Wait for reveal");
+        require(commitReveal.getSaltedHash(bytes32(choice), bytes32(salt)) == player[idx].commit, "Incorrect choice or salt");
 
         player[idx].choice = choice;
-        numInput++;
+        player[idx].isRevealed = true;
+        numReveal++;
 
-        if (numInput == 2) {
+        if (numReveal == 2) {
             _checkWinnerAndPay();
         }
     }
@@ -73,7 +95,7 @@ contract RWAPSSF {
         uint idx = playersNumber[msg.sender];
         require(idx != 0, "Registered player only");
         require(player[idx].timestamp + timeLimit < block.timestamp, "Please wait for 10 minutes before withdraw money back");
-        require(numPlayer == 1 || numInput < 2, "Please wait");
+        require(numPlayer == 1 || numInput < 2 || numReveal < 2, "Please wait");
         
         if (numPlayer == 1) {
             payable(player[idx].addr).transfer(reward);
